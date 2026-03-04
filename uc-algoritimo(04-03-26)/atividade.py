b0627de2297af0a5974f1301bf5136ee42f87651
CaioLeonardo14121 <caioleonardo804@gmail.com>
2026-03-04 11:18:48 -0300

    Add files via upload

---
+++ uc-algoritimo(04-03-26)/atividade.py
@@ -0,0 +1,10 @@
+compras = [
+    "arroz",
+    "feijão",
+    "leite",
+    "pão",
+    "ovos"
+]
+novo_item = input("Digite um intem!")
+compras.append(novo_item)
+print(f"Lista de Compras: {compras}")
